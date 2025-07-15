3d0733641dc53a4018f87fe8682e9be2e93848a0
lavish-shrma <lavishkumar3435@gmail.com>
2025-07-15 23:32:15 +0530

    ✨ feat: add JavaScript with UI interaction tweaks

--- js/main.js
+++ js/main.js
@@ -35,10 +35,51 @@ function toggleButton() {
         } else {
           cartButton.classList.add("active");
           cartPlusMinus.classList.remove("active");
+
+          const gridItem = cartButton.closest(".grid-item");
+          addToCart(gridItem);
         }
       }
     }
   });
+}
+
+function addToCart(gridItem) {
+  const itemName = gridItem.querySelector('.tertiary-header').innerText;
+  const itemPrice = gridItem.querySelector('.item-price').innerText;
+
+  const cartItem = document.createElement('article');
+  cartItem.classList.add('cart-item');
+  cartItem.innerHTML = `
+    <div class="cart-quantity">
+      <p class="cart-heading">${itemName}</p>
+      <div class="quantity-wrap">
+        <span class="quantity">1x</span>
+        <span class="each-item">${itemPrice}</span>
+        <span class="item-total">$5.50</span>
+      </div>
+    </div>
+    <button class="remove-item">
+      <span class="access-hidden">Remove Item</span>
+      <svg
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="10"
+        height="10"
+        fill="none"
+        viewBox="0 0 10 10"
+      >
+        <path
+          fill=""
+          d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"
+        />
+      </svg>
+    </button>
+  `;
+
+  // append to the sidebar
+  const cartSidebar = document.querySelector(".side-wrap");
+  cartSidebar.appendChild
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -121,74 +162,93 @@ document.addEventListener('DOMContentLoaded', () => {
       toggleButton();
     })
     .catch(error => {
-      console.log(`Error laoding JSON data: `, error);
+      console.log(`Error loading JSON data: `, error);
     });
 });
 
-/* <article class="grid-item">
-              <div class="button-container">
-                <figure class="image-container">
-                  <img
-                    src="assets/images/image-baklava-desktop.jpg"
-                    alt="image placeholder text"
-                  />
-                </figure>
-                <button class="cart-button">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="21"
-                    height="20"
-                    fill="none"
-                    viewBox="0 0 21 20"
-                  >
-                    <g fill="#C73B0F" clip-path="url(#a)">
-                      <path
-                        d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"
-                      />
-                      <path
-                        d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id="a">
-                        <path fill="#fff" d="M.333 0h20v20h-20z" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                  Add to Cart
-                </button>
-                <div class="cart-plus-minus active">
-                  <span class="access-hidden">Cart Quantity</span>
-                  <button class="more-less">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="10"
-                      height="2"
-                      fill="none"
-                      viewBox="0 0 10 2"
-                    >
-                      <path fill="" d="M0 .375h10v1.25H0V.375Z" />
-                    </svg>
-                  </button>
-                  <span class="item-quantity">1</span>
-                  <button class="more-less">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="10"
-                      height="10"
-                      fill="none"
-                      viewBox="0 0 10 10"
-                    >
-                      <path
-                        fill=""
-                        d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-              <div class="item-category">Creme Brulee</div>
-              <h3 class="tertiary-header">Vanilla Bean Creme Brulee</h3>
-              <div class="item-price">$7.00</div>
-            </article> 
-*/
+/* 
+<div class="full-cart">
+  <h2 class="secondary-header">Your Cart (7)</h2>
+  <div class="cart-container">
+    
+    <article class="cart-item">
+      <div class="cart-quantity">
+        <p class="cart-heading">Tiramisu</p>
+        <div class="quantity-wrap">
+          <span class="quantity">1x</span>
+          <span class="each-item">@$5.50</span>
+          <span class="item-total">$5.50</span>
+        </div>
+      </div>
+      <button class="remove-item">
+        <span class="access-hidden">Remove Item</span>
+        <svg
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="10"
+          height="10"
+          fill="none"
+          viewBox="0 0 10 10"
+        >
+          <path
+            fill=""
+            d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"
+          />
+        </svg>
+      </button>
+    </article>
+    <article class="cart-item">
+      <div class="cart-quantity">
+        <p class="cart-heading">Tiramisu</p>
+        <div class="quantity-wrap">
+          <span class="quantity">1x</span>
+          <span class="each-item">@$5.50</span>
+          <span class="item-total">$5.50</span>
+        </div>
+      </div>
+      <button class="remove-item">
+        <span class="access-hidden">Remove Item</span>
+        <svg
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="10"
+          height="10"
+          fill="none"
+          viewBox="0 0 10 10"
+        >
+          <path
+            fill=""
+            d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"
+          />
+        </svg>
+      </button>
+    </article>
+  </div>
+  <p class="order-total">
+    <span>Order Total:</span>
+    <span class="total-amount"> $46.50</span>
+  </p>
+  <div class="carbon-neutral">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="21"
+      height="20"
+      fill="none"
+      viewBox="0 0 21 20"
+    >
+      <path
+        fill="#1EA575"
+        d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"
+      />
+      <path
+        fill="#1EA575"
+        d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"
+      />
+    </svg>
+    <p>
+      This is a <strong>&nbsp;carbon-neutral&nbsp;</strong> delivery
+    </p>
+  </div>
+  <button class="confirm-order">Confirm Order</button>
+</div>
+  */
